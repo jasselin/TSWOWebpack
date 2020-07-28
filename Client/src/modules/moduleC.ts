737cfd0459df49213364f91f3c817f2dbd05dd15
Jason Asselin <jason.asselin@cdid.com>
2020-07-28 08:49:10 -0400

    Add another dependent module from dynamically loaded dependency

--- Client/src/modules/moduleC.ts
+++ Client/src/modules/moduleC.ts
@@ -1,6 +1,8 @@
 import { fn2 } from "./../common";
+import { otherfn } from "./../other"
 
 export default () => {
     console.log("Module C!");
     fn2();
+    otherfn();
 };
---
+++ Client/src/other.ts
@@ -0,0 +1,3 @@
+export function otherfn() {
+    console.log("other");
+}
